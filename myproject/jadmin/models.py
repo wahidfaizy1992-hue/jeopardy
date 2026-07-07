21188ff6f3886e55c4956bdcae1f35954a54abce
Wayne <wayne.largent@tri-c.edu>
2026-07-07 17:41:35 -0400

    model creation WEL

--- myproject/jadmin/models.py
+++ myproject/jadmin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    board_number = models.IntegerField()
+    category = models.CharField(max_length=20)
+    clue = models.CharField(max_length=255)
+    response = models.CharField(max_length=255)
+    value = models.IntegerField()
+    staus = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.category} - {self.clue} - {self.response} - {self.value}"
