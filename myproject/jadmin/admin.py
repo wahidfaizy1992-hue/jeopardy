32df9ea6194c68d8b7d54be69b49bec9df7dcfe0
Wayne <wayne.largent@tri-c.edu>
2026-07-13 17:36:30 -0400

    New Model and Board

--- myproject/jadmin/admin.py
+++ myproject/jadmin/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Board
+from .models import Category
+from .models import Clue
 
 # Register your models here.
 admin.site.register(Board)
+admin.site.register(Category)
+admin.site.register(Clue)
--- myproject/static/js/main.js
+++ myproject/static/js/main.js
@@ -1,2 +1,28 @@
 // main JS file 
-console.log("Hello World!");
+// console.log("Hello World!");
+
+function cClick(c) {
+    const modal = document.getElementById('clue-modal');
+        
+    // Disable the button so players can't click it again
+    // this.classList.add('used');
+    // this.disabled = true;
+
+    // Fetch the clue data from Django
+    fetch(`/clue/${c}/`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('modal-value').innerText = `$${data.value}`;
+            document.getElementById('modal-text').innerText = data.text;
+            document.getElementById('modal-answer').innerText = data.answer;
+            modal.style.display = 'block'; // Show the modal
+        });
+}
+
+function closeModal() {
+    document.getElementById('clue-modal').style.display = 'none';
+}
+
+function showAnswer() {
+    ;
+}
